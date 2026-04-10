827ffc010c99f6b6554bf38d95c0ce21065cd1fe
Admin <abilmansur2501@gmail.com>
2026-04-10 12:00:05 +0500

    cors error fix1

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,6 +26,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:5173",
+        "https://nesie.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
